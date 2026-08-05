58b4bdbf94dc91ffcb8a85dbf2b42bd35022627e
guiisilva00 <guilherme.p.silva51@aluno.senai.br>
2026-08-05 17:09:23 -0300

    Tentando replicar uma página da atividade

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,8 +4,23 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <>
-      <h1 className="chamativo">Testando papai</h1>
-      <p>Testando pt 2</p>    
-      </>
+      <header className="header-tier">
+        <div className="nome-empresa">Tier</div>
+        <nav>
+          <a href="#">What we do</a>
+          <a href="#">Blog</a>
+          <a href="#">Podcast</a>
+          <a href="#">Careers</a>
+          <a href="#" className="cta">Work with us</a>
+        </nav>
+      </header>
+
+      <section className="container">
+        <h1 className="principal-title">Unlock your business Potential With Facebook & Instagram Advertising</h1>
+        <p className="principal-subtitle">Facebook Premier Level Partner Agency</p>
+        <a href="#" className="cta" id="cta-principal">Work with us</a>
+        <img src="/imagens/foguetinho.png" alt="" srcset="" />
+      </section>
+    </>
   );
 }
