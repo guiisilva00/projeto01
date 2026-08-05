5343c884f9a4cdb82813332a9d0be3df59928ee3
guiisilva00 <guilherme.p.silva51@aluno.senai.br>
2026-08-05 16:06:20 -0300

    complemento pro projeto

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="chamativo">Testando papai</h1>
+      <p>Testando pt 2</p>    
+      </>
+  );
+}
